71313e0d6cf6c05f75dec935bd2ba5809673593b
Breno Garcia <BrenoGHeat@gmail.com>
2024-01-02 10:51:51 -0300

    resolvido

--- script.js
+++ script.js
@@ -3,22 +3,45 @@
   Mostre somente o elemento selecionado no console.
 */
 
+const meusIngredientes = document.getElementById("ingredients");
+console.log(meusIngredientes);
+
 /*
   2. Selecione o elemento <h1> e o atribua a uma constante.
    Mostre somente o elemento selecionado no console.
 */
+
+const meuTitulo = document.getElementsByTagName("h1")[0];
+console.log(meuTitulo);
 
 /*
   3. Selecione o elemento com classe "recipe" e o atribua a uma constante.
    Mostre somente elemento selecionado no console.
 */
 
+const recipientes = document.getElementsByClassName("recipe")[0];
+console.log(recipientes);
+
+
 /*
   Desafio 1: Selecione todos os elementos <h2> da página e altere a cor do texto de
    todos para azul.
 */
 
+const subTitles = document.getElementsByTagName("h2");
+
+ for( let i = 0 ; i < subTitles.length ; i++){
+  subTitles[i].style.color = "blue" ;
+ }
+
+
 /* 
   Desafio 2: Selecione todos os elementos <input> da página, e altere a cor da borda de
    todos para vermelho.
 */
+
+const inputs = document.querySelectorAll("input");
+
+for(let i = 0 ; i < inputs.length ; i++){
+  inputs[i].style.borderColor = "red" ;
+}
